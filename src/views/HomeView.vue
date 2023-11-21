50820423ccc2e0ea07263058823df8af849b2929
Dennis Herrera <dennishumher25@hotmail.com>
2023-11-21 12:48:16 -0600

    home view: added footer and main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,40 @@
 </script>
 
 <template>
-  <div>
-    <header class="mx-9 my-6 flex items-center justify-center relative">
+  <div class="my-8">
+    <header class="mx-10 flex items-center justify-center relative">
       <img
         src="/title.png"
         alt="Attack on titan"
-        class="w-40"
+        class="w-36 min-[350px]:w-40"
       >
       <p class="text-xs font-montserrat font-semibold uppercase absolute right-0">About</p>
     </header>
 
+    <main>
+      <HomeBody />
+    </main>
     
+    <footer class="pt-28 pb-4 px-12 flex flex-col items-center gap-6 font-open">
+      <button
+        class="text-sm underline"
+      >
+        Back to top
+      </button>
+
+      <a
+        href="https://github.com/Klonnister/attack-on-titan"
+        class="text-xs uppercase font-montserrat flex justify-center items-center gap-2"
+      >
+        <img
+          src="/github.png"
+          alt=""
+          class="w-8"
+        >
+        <span>Klonnister</span>
+      </a>
+
+      <p class="text-xs font-open text-justify" style="text-align-last: center">This site is not official, filiate nor funded by Kodansha. It was made with the sole purpose of fun and practicing.</p>
+    </footer>
   </div>
 </template> 
