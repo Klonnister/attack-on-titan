a18aee7e74378ac2c808288325efa1ac76158928
Dennis Herrera <dennishumher25@hotmail.com>
2023-11-22 00:00:28 -0600

    homepage: added responsiveness and img gray effect

--- src/components/HomeBody.vue
+++ src/components/HomeBody.vue
@@ -1,44 +1,54 @@
 <script setup>
 import { homeLinks } from '@/composables/useLinks'
+import { ref } from 'vue'
 
 const useHomeLinks = homeLinks
+
+const width = ref( window.innerWidth );
+const updateWidth = () => {
+    width.value = window.innerWidth
+}
+
+window.addEventListener('resize', updateWidth)
+
 </script>
 
 <template>
     <div class="flex flex-col">
-        <h1 class="mt-12 text-2xl text-[#D8D8D8] uppercase font-montserrat font-bold text-center">Shingeki no Kyojin</h1>
+        
+        <h1 class="mt-12 sm:mt-16 lg:mt-20 text-2xl lg:text-3xl text-[#D8D8D8] uppercase font-montserrat font-bold text-center">Shingeki no Kyojin</h1>
 
-        <img
-          src="/portrait.jpeg"
-          alt=""
-          class="w-full mt-10"
-        >
+        <div class="my-10 sm:my-16 text-sm md:text-base text-justify leading-8 sm:leading-9 md:leading-10">
+            <img
+                src="/portrait.jpeg"
+                alt=""
+                class="w-full sm:w-72 md:w-80 lg:w-96 mb-12 sm:mb-0 sm:ms-7 lg:ms-10 sm:float-right sm:py-2"
+            >
 
-        <div class="flex flex-col gap-7 my-10 px-10 text-sm text-justify leading-8">
-            <p>
+            <p class="px-10 min-[450px]:px-0 mb-10 sm:mb-6 md:mb-8">
                 Attack on Titan (Japanese: 進撃の巨人, Hepburn: Shingeki no Kyojin 'The Attacking Giant') is a Japanese manga series written and illustrated by 
 
                 <a
-                  href="https://en.wikipedia.org/wiki/Hajime_Isayama"
-                  class="text-[#FFFFFF]"
+                    href="https://en.wikipedia.org/wiki/Hajime_Isayama"
+                    class="text-[#FFFFFF]"
                 >
                     Hajime Isayama
                 </a>
                 .
             </p>
     
-            <p>
+            <p class="px-10 min-[450px]:px-0 mb-10 sm:mb-6 md:mb-8">
                 It is set in a post-apocalyptic world where the remains of humanity live behind walls protecting them from giant humanoid Titans, Attack on Titan follows protagonist Eren Yeager, along with friends Mikasa Ackerman and Armin Arlert. When a Colossal Titan breaches the wall of their hometown, Titans destroy the city and eat Eren's mother. Vowing vengeance, Eren joins the elite Survey Corps, a group of soldiers who fight against Titans. It chronicles Eren's journey with the Survey Corps as they fight against the Titans while investigating their origin and history.
             </p>
 
-            <p>
+            <p class="px-10 min-[450px]:px-0">
                 An anime television series was produced by Wit Studio (seasons 1-3) and MAPPA (season 4). The series premiered on April 7, 2013, and concluded on November 5, 2023.  In North America, the series has been streamed on Crunchyroll, Funimation, and Hulu. Attack on Titan has also aired on Adult Swim's Toonami programming block in the United States.
             </p>
         </div>
 
-        <div class="my-10 w-full text-xs text-center">
+        <div class="my-10 sm:my-16 w-full text-xs text-center flex flex-col gap-2">
             <iframe
-              class="w-full h-56"
+              class="w-full h-64 min-[475px]:h-96 lg:h-[28rem]"
               src="https://www.youtube.com/embed/LV-nazLVmgo?si=B41_14QtFp3KBHee"
               title="Youtube Video Player"
               frameborder="0"
@@ -58,35 +68,36 @@ const useHomeLinks = homeLinks
             </p>
         </div>
 
-        <div class="my-12 px-10 text-center flex flex-col gap-8">
-            <h2 class="font-montserrat font-bold text-xl text-[#D8D8D8] uppercase">What you'll find here</h2>
-            <p class="text-sm text-justify leading-8">Select one of the options below to start browsing. If you haven't watched the anime series and are planning to, you might get 'spoilers' in these sections, so beware.</p>
+        <div class="my-12 sm:my-16 lg:my-20 px-10 min-[450px]:px-0 text-center flex flex-col gap-8">
+            
+            <h2 class="font-montserrat font-bold text-xl md:text-2xl text-[#D8D8D8] uppercase">What you'll find here</h2>
+            <p class="text-sm md:text-base text-justify leading-8 sm:leading-9 md:leading-10 sm:text-center">Select one of the options below to start browsing. If you haven't watched the anime series and are planning to, you might get 'spoilers' in these sections, so beware.</p>
 
-            <div class="my-8 flex flex-col gap-12">
+            <div class="my-12 sm:mt-16 md:mt-20 grid grid-cols-1 sm:grid-cols-2 gap-12">
                 <router-link
                     v-for="link in useHomeLinks"
-                    :key="link.name"
+                    :key="link.id"
                     :to="link.url"
                 >
-                    <img
-                      class="w-full h-42 object-cover rounded-3xl mb-3"
-                      :src="link.img"
+                    <div
+                      :class="`w-full sm:h-36 md:h-48 lg:h-56 xl:h-64 hover:xl:h-[17rem] bg-no-repeat bg-cover bg-center rounded-3xl mb-3 transition-all duration-500 ease-out ${link.id}`"
                       alt=""
-                    >
-                    <p class="text-[13px] font-montserrat uppercase font-semibold">{{ link.name }}</p>
+                      :ref="link.id"
+                    ></div>
+                    <p class="text-[13px] min-[475px]:text-sm xl:text-base font-montserrat uppercase font-semibold">{{ link.name }}</p>
                 </router-link>
             </div>
         </div>
 
-        <div class="my-8 px-10 text-center flex flex-col gap-6 text-sm text-justify leading-8">
+        <div class="my-8 xl:my-12 px-10 min-[450px]:px-0 text-center flex flex-col gap-6 text-sm md:text-base text-justify leading-8 sm:leading-9 md:leading-10">
             <h2 class="font-montserrat font-bold text-center text-xl text-[#D8D8D8] uppercase">Where to watch</h2>
 
-            <div class="my-8 flex justify-center items-center gap-6">
+            <div class="my-8 md:my-12 xl:my-16 flex justify-center items-center gap-6 sm:gap-12 lg:gap-20 xl:gap-24">
                 <a href="https://www.crunchyroll.com/es-es/series/GR751KNZY/attack-on-titan">
-                    <img src="/crunchyroll.png" alt="" class="w-20 h-auto">
+                    <img src="/crunchyroll.png" alt="" class="w-20 sm:w-28 hover:sm:rotate-[-4deg] transition-all ease-in-out duration-300 h-auto">
                 </a>
                 <a href="https://www.amazon.com/-/es/gp/video/detail/B00NL5MRRU/ref=atv_dp_season_select_s101?language=es">
-                    <img src="/primevideo.png" alt="" class="w-32">
+                    <img src="/primevideo.png" alt="" class="w-32 sm:w-48 hover:sm:rotate-[-4deg] transition-all ease-in-out duration-300">
                 </a>
             </div>
 
@@ -96,3 +107,56 @@ const useHomeLinks = homeLinks
         </div>
     </div>
 </template>
+
+<style scoped>
+.characters {
+    background-image: url('/characters.gif');
+}
+
+.episodes {
+    background-image: url('/episodes.gif');
+}
+
+.locations {
+    background-image: url('/locations.gif');
+}
+
+.titans {
+    background-image: url('/titans.gif');
+}
+
+@media screen and (min-width: 1280px) {
+    .characters {
+        background-image: url('/characters-gray.jpg');
+    }
+
+    .characters:hover {
+        background-image: url('/characters.gif');
+    }
+
+    .episodes {
+        background-image: url('/episodes-gray.jpg');
+    }
+
+    .episodes:hover {
+        background-image: url('/episodes.gif');
+    }
+
+    .locations {
+        background-image: url('/locations-gray.jpg');
+    }
+
+    .locations:hover {
+        background-image: url('/locations.gif');
+    }
+
+    .titans {
+        background-image: url('/titans-gray.jpg');
+    }
+
+    .titans:hover {
+        background-image: url('/titans.gif');
+    }
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,23 +5,25 @@ const scrollToTop = useScroll
 </script>
 
 <template>
-  <div class="my-8">
-    <header class="mx-10 flex items-center justify-center relative">
+  <div class="my-8 lg:my-12 max-w-[450px] min-[450px]:px-10 lg:px-20 xl:px-32 sm:container mx-auto">
+    <header class="mx-10 min-[450px]:mx-0 flex items-center justify-center relative">
       <img
         src="/title.png"
         alt="Attack on titan"
-        class="w-36 min-[350px]:w-40"
+        class="w-36 min-[350px]:w-40 sm:w-72 lg:w-80"
       >
-      <p class="text-xs font-montserrat font-semibold uppercase absolute right-0">About</p>
+      <p class="text-xs sm:text-sm lg:text-base font-montserrat font-semibold uppercase absolute right-0 text-[#D8D8D8]">
+        About
+      </p>
     </header>
 
     <main>
       <HomeBody />
     </main>
     
-    <footer class="pt-28 pb-4 px-10 flex flex-col items-center gap-6">
+    <footer class="pt-28 pb-4 px-10 min-[450px]:px-0 flex flex-col items-center gap-6 xl:gap-5">
       <button
-        class="text-sm underline"
+        class="text-sm md:text-base underline"
         @click="scrollToTop"
       >
         Back to top
@@ -29,17 +31,17 @@ const scrollToTop = useScroll
 
       <a
         href="https://github.com/Klonnister"
-        class="text-xs uppercase font-montserrat flex justify-center items-center gap-2"
+        class="text-xs md:text-sm lg:text-base uppercase font-montserrat flex justify-center items-center gap-2 md:gap-4"
       >
         <img
           src="/github.png"
           alt=""
-          class="w-8"
+          class="w-8 md:w-10"
         >
         <span>Klonnister</span>
       </a>
 
-      <p class="text-xs text-justify last-center" >This site is not official, filiate nor funded by Kodansha. It was made with the sole purpose of fun and practicing.</p>
+      <p class="text-xs lg:text-sm text-justify last-center" >This site is not official, filiate nor funded by Kodansha. It was made with the sole purpose of fun and practicing.</p>
     </footer>
   </div>
 </template> 
