5e960f3644deb03b2aee86a83d5cb5ae40c2e64b
Dennis Herrera <dennishumher25@hotmail.com>
2023-11-23 16:11:19 -0600

    separated footer

---
+++ src/components/shared/MainFooter.vue
@@ -0,0 +1,31 @@
+<script setup>
+import useScroll from '@/composables/useScroll'
+
+const scrollToTop = useScroll
+</script>
+
+<template>
+    <footer class="pt-28 pb-4 flex flex-col items-center gap-6 xl:gap-5">
+        <button
+          class="text-sm md:text-base hover:lg:text-[17px] transition-all duration-300 ease-in-out underline"
+          @click="scrollToTop"
+        >
+            Back to top
+        </button>
+
+        <a
+          href="https://github.com/Klonnister"
+          class="text-xs md:text-sm lg:text-base hover:lg:text-[17px] transition-all duration-300 ease-in-out uppercase font-montserrat flex justify-center items-center gap-2 md:gap-4"
+        >
+            <img
+              src="/github.png"
+              alt=""
+              class="w-8 md:w-10"
+            >
+
+            <span>Klonnister</span>
+         </a>
+
+         <p class="text-xs lg:text-sm text-justify last-center" >This site is not official, filiate nor funded by Kodansha. It was made with the sole purpose of fun and practicing.</p>
+    </footer>
+</template>
--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,10 +1,3 @@
-<script setup>
-import useScroll from '@/composables/useScroll'
-
-const scrollToTop = useScroll
-</script>
-
-
 <template>
 
 <div class="my-8 lg:my-12 max-w-[450px] px-12 lg:px-20 xl:px-32 sm:container mx-auto">
@@ -25,28 +18,8 @@ const scrollToTop = useScroll
 
     <slot/>
 
-    <footer class="pt-28 pb-4 flex flex-col items-center gap-6 xl:gap-5">
-      <button
-        class="text-sm md:text-base hover:lg:text-[17px] transition-all duration-300 ease-in-out underline"
-        @click="scrollToTop"
-      >
-        Back to top
-      </button>
+    <MainFooter />
 
-      <a
-        href="https://github.com/Klonnister"
-        class="text-xs md:text-sm lg:text-base hover:lg:text-[17px] transition-all duration-300 ease-in-out uppercase font-montserrat flex justify-center items-center gap-2 md:gap-4"
-      >
-        <img
-          src="/github.png"
-          alt=""
-          class="w-8 md:w-10"
-        >
-        <span>Klonnister</span>
-      </a>
-
-      <p class="text-xs lg:text-sm text-justify last-center" >This site is not official, filiate nor funded by Kodansha. It was made with the sole purpose of fun and practicing.</p>
-    </footer>
   </div>
 
 </template>
