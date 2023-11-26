71f52d9d7b36dab1ca0789c38434cb4452ec4a0d
Dennis Herrera <dennishumher25@hotmail.com>
2023-11-25 19:20:04 -0600

    characters: added characters cards

--- src/components/shared/CharacterCard.vue
+++ src/components/shared/CharacterCard.vue
@@ -1,13 +1,36 @@
 <script setup>
-    const props = defineProps({
-        name: { type: String, required: true },
-        img: { type: String, required: true },
-    })
+import { ref } from 'vue'
+
+const props = defineProps({
+    name: { type: String, required: true },
+    img: { type: String, required: true },
+    status: { type: String, required: true },
+})
+
+const fixedLink = ref(null)
+
+if( props.img ) {
+    fixedLink.value = props.img.slice(0, (props.img.indexOf('revision') - 1))
+}
+    
 </script>
 
 <template>
-    <div>
-        <img :src="props.img" alt="">
-        <p>{{ props.name }}</p>
+    <div class="flex flex-col gap-2 items-center">
+        <img
+          v-if="props.img"
+          :src="fixedLink"
+          alt=""
+          :class="props.status === 'Deceased' ? 'grayscale px-2' : ' px-2'"
+        >
+
+        <div
+            class="h-full w-full text-center flex items-center justify-center"
+            v-else
+        >
+            <p class="text-sm font-martel">No image</p>
+        </div>
+
+        <p class="text-xs font-martel">{{ props.name }}</p>
     </div>
 </template>
--- src/views/CharactersView.vue
+++ src/views/CharactersView.vue
@@ -2,31 +2,30 @@
 import { ref } from 'vue'
 
 const characters = ref(null)
-const character = ref(null)
 
 const getCharacters = async () => {
-    const resp = await fetch('https://api.attackontitanapi.com/characters').then( r => r.json() )
+    const response = await fetch('https://api.attackontitanapi.com/characters').then( r => r.json() )
 
-    characters.value = resp
-    character.value = characters.value.results[1]
+    characters.value = response.results
 }
+
 getCharacters()
+
 
 </script>
 
 <template>
     
     <div v-if="characters">
-        <h2 class="text-xl sm:text-2xl lg:text-3xl text-[#D8D8D8] uppercase font-montserrat font-bold text-center mb-8">Characters</h2>
+        <h2 class="text-xl sm:text-2xl lg:text-3xl text-[#D8D8D8] uppercase font-montserrat font-bold text-center">Characters</h2>
 
-        <p class="text-sm md:text-base text-justify leading-8 sm:leading-9 md:leading-10 mb-10 sm:mb-6 md:mb-8">Attack on Titan series feature an extensive cast of fictional characters created by Hajime Isayama. The story initially centers on Eren Yeager with his childhood friends Mikasa Ackerman and Armin Arlert, who join the military to fight the Titans. They are part of the 104th Training Corps, whose graduates assume different positions in the Military, including the Garrison Regiment, the Survey Corps and the Military Police Brigade.</p>
-
-        <p class="text-sm md:text-base text-justify leading-8 sm:leading-9 md:leading-10 mb-10 sm:mb-6 md:mb-8">It is later revealed that the area where the Walls are located is called Paradis and that there are other nations outside the walls of their mother womb, namely Marley which has infiltrated Paradis.</p>
-
-        <div>
+        <div class="py-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 sm:gap-10">
             <CharacterCard 
+                v-for="character in characters"
+                :key="character.id"
                 :name="character.name"
-                :img="character.img"
+                :img="character.img || '' "
+                :status="character.status"
             />
         </div>
     </div>
