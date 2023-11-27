54a19bc01da91f86c6fdaebb679723ee7d0b9256
Dennis Herrera <dennishumher25@hotmail.com>
2023-11-27 16:56:31 -0600

    added rough scroll method

--- src/composables/useScroll.js
+++ src/composables/useScroll.js
@@ -2,4 +2,8 @@ const scrollToTop = () => {
     window.scrollTo({top: 0, behavior: 'smooth'})
 }
 
+export const scrollToTopRough = () => {
+    window.scrollTo({top: 0})
+}
+
 export default scrollToTop
