2e622173716da1d21b050720f3925c2dcf862167
Dennis Herrera <dennishumher25@hotmail.com>
2023-11-27 22:47:21 -0600

    fixed no found image error

--- src/components/shared/CharacterCard.vue
+++ src/components/shared/CharacterCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 const props = defineProps({
     name: { type: String, required: true },
@@ -7,30 +7,33 @@ const props = defineProps({
     status: { type: String, required: true },
 })
 
-const fixedLink = ref(null)
+const fixedLink = ref(null);
 
-if( props.img ) {
-    fixedLink.value = props.img.slice(0, (props.img.indexOf('revision') - 1))
+if ( props.name === 'Muller' ) {
+    fixedLink.value = null;
 }
-    
+else if( props.img ) {
+    fixedLink.value = props.img.slice(0, (props.img.indexOf('revision') - 1));
+}
+
 </script>
 
 <template>
-    <div class="flex flex-col gap-2 items-center">
+    <div class="flex flex-col gap-2 items-center h-full">
         <img
-          v-if="props.img"
+          v-if="fixedLink"
           :src="fixedLink"
           alt=""
-          :class="props.status === 'Deceased' ? 'grayscale px-2 rounded-md' : 'px-2 rounded-md'"
+          :class="props.status === 'Deceased' ? 'grayscale px-2 rounded-md grow' : 'px-2 rounded-md grow'"
         >
 
         <div
-            class="h-full w-full text-center flex items-center justify-center"
+            class="h-full w-full text-center flex items-center justify-center py-12 bg-[#202020]"
             v-else
         >
             <p class="text-sm font-martel">No image</p>
         </div>
 
-        <p class="text-xs font-martel text-center">{{ props.name }}</p>
+        <p class="text-xs lg:text-sm font-martel text-center">{{ props.name }}</p>
     </div>
 </template>
